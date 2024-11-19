7b72c66222d9cae673feb8461e4b7facfcc3a9ad
Iebs16 <aryan.tiwari@iebrain.com>
2024-11-19 16:16:14 +0530

    chatbot masti

--- backend/Utilities/diseasechatbot.py
+++ backend/Utilities/diseasechatbot.py
@@ -21,7 +21,8 @@ keys = [
     'Safety',
     'Adverse_Events',
     'Annual_Therapy_Costs',
-    'Type_of_Drug'
+    'Type_of_Drug',
+    'Country'
 ]
 
 
@@ -62,7 +63,7 @@ def create_prompt(search_results):
     Returns:
     - A formatted OpenAI prompt string.
     """
-    prompt = "Here are the top search results for your query:\n\n"
+
     disease_data = search_results['diseaseData'][0]
     drug_data = search_results['drugData']
     filtered_drug_data = filter_keys(drug_data, keys)
@@ -134,7 +135,7 @@ def process_question(results, question, conversation_history):
 if __name__ == "__main__":
     print("Welcome to the Disease Chatbot!")
     search_results = {'diseaseData': [{'Disease': 'Waldenstrom Macroglobulinemia','Disease Overview': 'Waldenstrom macroglobulinemia is a rare type of blood cancer, often affecting individuals aged 65 and older. Symptoms may include fatigue, fever, weight loss, night sweats, and enlarged lymph nodes.','Diagnosis': 'Diagnosis involves laboratory tests, including electrophoresis, bone marrow biopsy, and imaging.', 'Treatment & Management': 'Options include chemotherapy, immunotherapy, and targeted therapy. In cases of blood thickening, plasmapheresis may be recommended.', 'Prevalence': 'Approximately 5 cases per 1 million people in the United States.'}], 'drugData': [{'TradeName': 'Brukinsa', 'Type_of_Drug': 'Bruton tyrosine kinase inhibitor', 'Active Ingredient': 'Zanubrutinib', 'Efficacy': '80% overall response rate in treating Waldenstrom macroglobulinemia.', 'Adverse_Events': 'Common side effects include diarrhea, fatigue, and bruising.', 'Annual_Therapy_Costs': 'CHF 40,000 - CHF 70,000'}]}
-    # search_results = {'diseaseData':[{}], 'drugData':[{}]}
+    # search_results = {'diseaseData':[{}], 'drugData':[{},{},{}]}
     while(True):
         question = input("You: ")
         if question == "exit":
