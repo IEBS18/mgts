0cccca6499f4f6fef2ba062118df4e14d9989d63
Nirmiti <nirmiti.deshmukh@iebrain.com>
2025-03-13 16:17:26 +0530

    drug type change to speciality  on frontend{abad}

--- client/src/components/Formulary/FormularyDrugPage.jsx
+++ client/src/components/Formulary/FormularyDrugPage.jsx
@@ -641,7 +641,7 @@ const FormularyDrugPage = () => {
                 {/* If present, show Drug Type */}
                 {drugItem["Drug Type"] !== "Not Available" && (
                   <p>
-                    <strong className="font-semibold">Drug Type:</strong>{" "}
+                    <strong className="font-semibold">Specialty:</strong>{" "}
                     {drugItem["Drug Type"]}
                   </p>
                 )}
